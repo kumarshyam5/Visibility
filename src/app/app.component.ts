6d78b756d73794217914bb9154a8167877849f31
syamkumar <kumarshyam5@gmail.com>
2017-05-20 12:54:19 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  StoreDetails:any;
+
+  constructor(private http:Http){
+    this.http.get('/src/app/sample.json')
+    .map(res=>res.json())
+    .subscribe(data=>this.StoreDetails=data);
+  }
 }
